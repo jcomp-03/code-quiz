cb951c757e920109fdd1f8d6b953636abadd739c
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-01-23 23:38:42 -0500

    Finished building out core functionality. Need to code out saving user information and score, and retrieving saved scores.

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,25 +25,29 @@ var questionObjectArray = []; // hold instances of object Question
 var fileDataArray = [] // hold split string from var data
 var numQuestionObjects; // defined in storeFileData()
 var questionIndex = 0; // position in object array
-var userScore = 100;
+var userScore = 100; // begin timer at 100
+var myIntervalId;
 
+// variable references to HTML elements
 var quizMainEl = document.querySelector("#id-quiz-main");
 var quizWrapperEl = document.querySelector("#id-quiz-wrapper");
 var startButtonEl = document.querySelector("#id-start-my-quiz");
 var choiceListEl = document.createElement("ul");
+var quizFooterEl = document.querySelector("footer");
 var timerEl = document.getElementById("timer-element");
 
+var positiveResponse = ["Correct! Good job.", "Nice! You are correct.", "That's correct!"];
 
 // string variables which holds the quiz questions
 var data = "Commonly used data types do NOT include:,alerts,strings,booleans,numbers,"+
-"The condition in an if/else statement is enclosed with:,quotes,curly brackets,parenthesis,"+
+"The condition in an if/else statement is enclosed with:,parenthesis,quotes,curly brackets,"+
 "square brackets,Arrays in JavaScript can be used to store:,all choices,numbers and"+
 " strings,other arrays,booleans,String values must be enclosed with ___ when being assigned"+
-" to variables., quotes, commas, curly brackets, parenthesis,A very useful tool used during"+
-" development and debugging for printing content to the debugger is:,console.log,JavaScript,"+
+" to variables.,quotes,commas,curly brackets, parenthesis,A very useful tool used during"+
+" development and debugging for printing content to the debugger is:,console log,JavaScript,"+
 "terminal/bash,for loops"
 
-// Constructor function
+// class declaration object Question
 class Question {
     constructor(interrogative, answRight, answWrong1, answWrong2, answWrong3) {
         this.interrogative = interrogative;
@@ -56,45 +60,62 @@ class Question {
 
 gradeSelectedChoice = function(e) {
     // debugger;
-
     var clickedEl = e.target;
-    var userSelection = "";
-    console.log('The clicked element is', clickedEl);
-
-    // check if event target matches <li> element
-    if (clickedEl.matches('li')) {
-        userSelection = clickedEl.textContent.split('.')[1].trim();
-        // compare <li> textContent value to the property answRight of the current object
-        if (userSelection === questionObjectArray[questionIndex].answRight) {
-            console.log("The user selected correctly!");
+    // execute block statement while the user still has time
+    // while (userScore > 0) {
+        // check if event target matches <li> element
+        if (clickedEl.matches('li')) {
+            // store <li>'s text in variable userSelection
+            var userSelection = clickedEl.textContent.split('.')[1].trim();
+            // compare <li> textContent value to the property answRight of the current object
+            if (userSelection === questionObjectArray[questionIndex].answRight) {
+                showRight(quizFooterEl);
+            } else {
+                showWrong(quizFooterEl);
+                userScore -= 10;
+            }
+            // increment counter by 1 to move to next object in questionObjectArray
+            questionIndex++;
+            
+            // logic to determine if more questions remain to be asked
+            if (questionIndex < numQuestionObjects){
+                createQuestionAndChoices(quizWrapperEl);
+            } else {
+                // user has cycled through all the quiz questions. End game and show results
+                return showScoreAndSaveToLocal(quizWrapperEl);
+            }
         } else {
-            userScore -= 15;
+            console.log("The user did not select an <li> element");
+            alert('Please select a choice');
         }
+    // }
+    // setTimeout(timeExpired(), 5000);
 
-        questionIndex++;
-        // ADD CODE TO DETERMINE IF MORE QUESTIONS REMAIN
-        createQuestionAndChoices(quizWrapperEl, questionIndex);
-    } else {
-        console.log("The user did not select an <li> element");
-        alert('Please select a choice');
-        return false;
-    }
+
 }
 
-createQuestionAndChoices = function(container) {
-     // clear out div container of any elements
-    container.replaceChildren();
+
+// timeExpired = function (container) {
+//     if (userScore == 0) {
+//         console.log('This console.log ran through the timeExpired() function.');
+//     }
+// }
     
-    // create dynamically <h1> element and append to div wrapper
+
+createQuestionAndChoices = function(container) {
+    
+    // clear out div container and ul of any children
+    container.replaceChildren();
+    choiceListEl.replaceChildren(); 
+
+    // dynamically create <h1> element
     var questionTitle = document.createElement("h1");
-    questionTitle.setAttribute("data-index-question-object", questionIndex);
+    questionTitle.setAttribute("data-index-question-object", questionIndex); // IN RETROSPECT DID NOT NEED CUSTOM DATA ATTRIBUTE!
     questionTitle.className = "title-question";
     questionTitle.innerText = questionObjectArray[questionIndex].interrogative;
 
-    // modify choiceListEl and append to div wrapper
     choiceListEl.setAttribute("data-index-question-object", questionIndex);
-    choiceListEl.className = "style-choice-list";
-    choiceListEl.textContent = "This is <ul> number " + questionIndex;
+    choiceListEl.className = "style-choice-list crosshair";
 
     // repopulate this array after iterating through an individual object's properties
     // since it is gradually reduced to an empty array at the end of the next for loop
@@ -104,7 +125,7 @@ createQuestionAndChoices = function(container) {
         // get some random number to cycle through object properties
         someRandomNum = randomNumber(0, propertiesArray.length-1);
         
-        // create dynamically <ul> and append to div wrapper
+        // create dynamically <li> and append to choiceListEl
         choiceListItem = document.createElement("li");
         choiceListItem.setAttribute("data-index-question-object", questionIndex);
         choiceListItem.className = "style-choice-list-item hand-pointer background-change";
@@ -117,24 +138,81 @@ createQuestionAndChoices = function(container) {
     }
 
     // Append <h1> and <ul> here
-    container.append(questionTitle,choiceListEl);
+    container.append(questionTitle, choiceListEl);
+}
+
+showRight = function(container) {
+    // clear the footer of children first
+    quizFooterEl.replaceChildren();
+    // create <h2> to display grade comment
+    var choiceResultMessage = document.createElement("h2");
+    choiceResultMessage.textContent = positiveResponse[randomNumber(0,positiveResponse.length-1)];
+    choiceResultMessage.className = "choice-result-message";
+    container.appendChild(choiceResultMessage);
+}
+showWrong = function(container) {
+    // clear the footer of children first
+    quizFooterEl.replaceChildren();
+    // create <h2> to display grade comment
+    var choiceResultMessage = document.createElement("h2");
+    choiceResultMessage.textContent = "Wrong";
+    choiceResultMessage.className = "choice-result-message font-red";
+    container.appendChild(choiceResultMessage);
 }
 
 // callback function handleStartQuiz()
 handleStartQuiz = function() {
     // let the quiz begin!
     console.log("The Start Quiz button has been pressed!");
+    
+    // create question and choices to begin
     createQuestionAndChoices(quizWrapperEl);
 }
 
+showScoreAndSaveToLocal = function(container) {
+    setTimeout( function(){
+        if(myIntervalId) {
+            clearInterval(myIntervalId);
+        }
+        container.replaceChildren();
+        quizFooterEl.replaceChildren();
+        var divWrapper = document.createElement('div');
+        var titleEl = document.createElement('h3');
+        var scoreEl = document.createElement('p');
+        var inputEl = document.createElement('input');
+        var inputLabelEl = document.createElement('label');
+        divWrapper.className = "end-game-save-user-info"
+        inputEl.setAttribute("id", "user-initials");
+        inputEl.setAttribute("type", "text");
+        inputEl.className = "end-game-input";
+        inputEl.setAttribute("placeholder", "Enter your first and last name here");
+        inputLabelEl.setAttribute("for", "user-initials");
+        inputLabelEl.className = "end-game-input-label"
+        inputLabelEl.innerText = "Enter your name here:"
+        titleEl.className = "title-question";
+        titleEl.innerText = "You finished the quiz OR the quiz time is expired!"
+        scoreEl.className = "quiz-paragraph end-game-score-text";
+        scoreEl.textContent = "Your score is: " + userScore + ".";
+        
+        // append elements to container here
+        divWrapper.append(inputLabelEl,inputEl);
+        container.append(titleEl, scoreEl, divWrapper);
+    }, 500);
+}
+
 beginTimer = function() {
-    var myIntervalId = setInterval(() => {
+    // run the timer down from 100
+    myIntervalId = setInterval(() => {
         userScore--;
         timerEl.textContent = userScore;
-        if (userScore === 0){
+        // if at any point the userScore is 0 or < 0, then set userScore to 0 and run
+        // the function showScoreAndSaveToLocal()
+        if (userScore === 0 || userScore < 0){
             clearInterval(myIntervalId);
+            userScore = 0;
             timerEl.textContent = "Time expired";
             // add code to alert user time is up and give back score
+            showScoreAndSaveToLocal(quizWrapperEl);
         }
     }, 1000);
 }
@@ -159,7 +237,7 @@ storeFileData = function() {
         // push the object instance to the array quizQuestionsArray
         questionObjectArray.push(myQuestion);
     }
-    // console.log(quizQuestionsArray);
+
 }
 
 // generate random number between min and max inclusive
@@ -177,6 +255,7 @@ startButtonEl.addEventListener('click', beginTimer);
 choiceListEl.addEventListener('click', gradeSelectedChoice);
 
 storeFileData();
+// timeExpired();
 
 /*
 ***** USER STORY *****
