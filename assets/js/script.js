ee6b97fc6fc29acbbf461a27baafaa4c81a75d73
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-01-28 11:46:31 -0500

    Finished view high scores functionality.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,13 +8,14 @@ var userScoreArray = [] // hold user scores
 var myIntervalId;
 
 // variable references to HTML elements
-var quizWrapperEl = document.querySelector("#id-quiz-wrapper");
+var divWrapperEl = document.querySelector("#id-div-wrapper");
 var startButtonEl = document.querySelector("#id-start-my-quiz");
 var submitButtonEl = document.createElement("button");
 var nameInputEl = document.createElement('input');
 var choiceListEl = document.createElement("ul");
 var quizFooterEl = document.querySelector("footer");
-var timerEl = document.getElementById("timer-element");
+var timerEl = document.getElementById("id-timer-element");
+var viewHighScoreEl = document.querySelector("header p");
 
 var positiveResponse = ["Good job!", "Nice! You're correct.", "Well done!", "That's correct!"];
 
@@ -56,10 +57,10 @@ gradeSelectedChoice = function(e) {
             
             // logic to determine if more questions remain to be asked
             if (questionIndex < numQuestionObjects){
-                createQuestionAndChoices(quizWrapperEl);
+                createQuestionAndChoices();
             } else {
                 // user has cycled through all the quiz questions. End game and show results
-                return showScoreAndSaveToLocal(quizWrapperEl, quizFooterEl);
+                return showScore(divWrapperEl, quizFooterEl);
             }
         // if the thing clicked on is not an <li> element, alert the user
         } else {
@@ -69,35 +70,27 @@ gradeSelectedChoice = function(e) {
         }
 }
 
-createQuestionAndChoices = function(container) {
-    
+createQuestionAndChoices = function() {
     // clear out div container and ul of any children
-    container.replaceChildren();
-    choiceListEl.replaceChildren(); 
-
+    divWrapperEl.replaceChildren();
+    choiceListEl.replaceChildren();
     // dynamically create <h1> element
     var questionTitle = document.createElement("h1");
-    questionTitle.setAttribute("data-index-question-object", questionIndex); // IN RETROSPECT DID NOT NEED CUSTOM DATA ATTRIBUTE!
     questionTitle.className = "title-question";
     questionTitle.innerText = questionObjectArray[questionIndex].interrogative;
-
-    choiceListEl.setAttribute("data-index-question-object", questionIndex); // IN RETROSPECT DID NOT NEED CUSTOM DATA ATTRIBUTE!
     choiceListEl.className = "style-choice-list crosshair";
-
     // repopulate this array after iterating through an individual object's properties
-    // since it is gradually reduced to an empty array at the end of the next for loop
+    // since it is gradually reduced to an empty array at the end of the for loop below
     var propertiesArray = ["answRight", "answWrong1", "answWrong2", "answWrong3"];
     
     for (let j = 0; j < 4; j++) {
         // get some random number to cycle through object properties
         someRandomNum = randomNumber(0, propertiesArray.length-1);
-        
         // create dynamically <li> and append to choiceListEl
         choiceListItem = document.createElement("li");
         choiceListItem.setAttribute("data-index-question-object", questionIndex);
         choiceListItem.className = "style-choice-list-item hand-pointer background-change";
         choiceListEl.appendChild(choiceListItem);
-
         // assign <li> innerText property to value of the randomly-selected current object property
         choiceListItem.innerText = (j+1) + '. ' + questionObjectArray[questionIndex][propertiesArray[someRandomNum]]
         // remove that currently-selected random property from the propertiesArray
@@ -105,7 +98,7 @@ createQuestionAndChoices = function(container) {
     }
 
     // Append <h1> and <ul> here
-    container.append(questionTitle, choiceListEl);
+    divWrapperEl.append(questionTitle, choiceListEl);
 }
 
 showRight = function(footer) {
@@ -128,16 +121,16 @@ showWrong = function(footer) {
     footer.appendChild(choiceResultMessage);
 }
 
-// callback function handleStartQuiz()
-handleStartQuiz = function() {
+// callback function startQuiz() called when start button is clicked
+startQuiz = function() {
     // let the quiz begin!
     console.log("The Start Quiz button has been pressed!");
     // create question and choices to begin
-    createQuestionAndChoices(quizWrapperEl);
+    createQuestionAndChoices();
 }
 
 // run this when timer finishes or user answers all quiz questions
-showScoreAndSaveToLocal = function(container, footer) {
+showScore = function(container, footer) {
     setTimeout( function() {
         // clear out the timer id value created when the setTimeout
         // function runs initially in the function beginTimer()
@@ -145,12 +138,11 @@ showScoreAndSaveToLocal = function(container, footer) {
         if(myIntervalId) {
             clearInterval(myIntervalId);
         }
-        // clear out the quizWrapperEl and quizFooterEl (if it even exists)
+        // clear out the quizWrapperEl and quizFooterEl (if latter even exists)
         container.replaceChildren();
         if(footer != undefined){
             footer.replaceChildren();
         }
-        
         // create the wrapper div to hold the remaining elements below
         var divWrapper = document.createElement('div');
         divWrapper.className = "end-game-save-user-info";
@@ -166,43 +158,41 @@ showScoreAndSaveToLocal = function(container, footer) {
         var inputLabelEl = document.createElement('label');
         inputLabelEl.setAttribute("for", "user-initials");
         inputLabelEl.className = "end-game-input-label";
-        inputLabelEl.innerText = "Enter your name here:";
+        inputLabelEl.innerText = "Enter name here:";
 
         nameInputEl.setAttribute("id", "user-initials");
         nameInputEl.setAttribute("type", "text");
         nameInputEl.className = "end-game-input";
-        nameInputEl.setAttribute("placeholder", "Enter your first and last name here");
+        nameInputEl.setAttribute("placeholder", "First and last name");
 
         submitButtonEl.setAttribute("id", "id-submit-user-info");
         submitButtonEl.setAttribute("type", "submit");
         submitButtonEl.className = "end-game-button-submit";
         submitButtonEl.innerText = "Submit";
-
         // append elements to container here
         divWrapper.append(inputLabelEl, nameInputEl, submitButtonEl);
         container.append(titleEl, scoreEl, divWrapper);
-
     }, 500);
 }
 
+// callback function beginTimer() called when start button is clicked
 beginTimer = function() {
     // run the timer down from 100
     myIntervalId = setInterval(() => {
         userScore--;
         timerEl.textContent = userScore;
         // if at any point the userScore is 0 or < 0, then set userScore to 0 and run
-        // the function showScoreAndSaveToLocal()
+        // the function showScore()
         if (userScore === 0 || userScore < 0) {
             clearInterval(myIntervalId);
             userScore = 0;
             timerEl.textContent = "Time expired";
-            // add code to alert user time is up and give back score
-            showScoreAndSaveToLocal(quizWrapperEl);
+            showScore(divWrapperEl);
         }
     }, 1000);
 }
 
-// split the large string var data and store it into fileDataArray. Use the array to populate my object Question instances
+// split data and store it into fileDataArray. Use the array to populate my object Question instances
 storeFileData = function() {
     // populate empty array
     fileDataArray = data.split(",");
@@ -225,26 +215,27 @@ storeFileData = function() {
     }
 }
 
-// generate random number between min and max inclusive
+// generate random number between min and max inclusive. I love this function.
 function randomNumber(min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
 }
 
+// save the user's score to local storage
 saveUserScore = function(e) {
-    if(e.target.matches("#id-submit-user-info")) {
-        console.log("The submit button has been pressed");
-        
+    if(e.target.matches("#id-submit-user-info")) { 
         var userInfo = nameInputEl.value;
-        console.log(userInfo, typeof userInfo);
-
         // check that user entered something
         if (userInfo && userInfo != "") {
+            // push the name and score to the object array userScoreArray
             userScoreArray.push( {
                 name: userInfo,
                 score: userScore
             });
+            // save to local storage
             localStorage.setItem("savedScores", JSON.stringify(userScoreArray));
+            // go to the quiz main page
+            goToMainPage(divWrapperEl);
         } else {
             alert("Please enter your name in the input field");
             return false;
@@ -254,27 +245,98 @@ saveUserScore = function(e) {
     }
 }
 
+// load saved scores from local storage
 loadUserScores = function() {
-    // debugger;
     var savedUserScores = localStorage.getItem("savedScores");
     // if there are no scores to load from local storage, print to console and exit function
     if (!savedUserScores) {
-        return console.log("There are no saved user scores to load");
+        return console.log("From loadUserScores(): There are no saved user scores to load");
     }
-    console.log("Saved user scores have been loaded");
-    // parse savedUserScores into an array of objects that we have already declared globally
+    console.log("From loadUserScores(): Saved user scores have been loaded");
+    // parse savedUserScores into our globally-defined array of objects
     userScoreArray = JSON.parse(savedUserScores);
 };
 
+goToMainPage = function() {
+    divWrapperEl.replaceChildren();
+    // dynamically create <h1> element
+    var pageTitle = document.createElement("h1");
+    pageTitle.className = "title-question";
+    pageTitle.innerText = "Coding Quiz Challenge";
+    // dynamically create <p> element
+    var pageParagraph = document.createElement("p");
+    pageParagraph.className = "quiz-paragraph";
+    pageParagraph.innerHTML = 
+            "Try to answer the following code-related questions within the time limit.<br>"+
+            "Keep in mind that incorrect answers will penalize your score/time.<br>"+
+            "by ten seconds!"
+    // reassign classes, attributes, text to startButtonEl declared globally
+    startButtonEl.className = "button-start-quiz hand-pointer background-change";
+    startButtonEl.setAttribute("type", "button");
+    startButtonEl.setAttribute("id", "id-start-my-quiz");
+    startButtonEl.innerText = "Start Quiz";
+    // append elements to div wrapper
+    divWrapperEl.append(pageTitle, pageParagraph, startButtonEl);
+}
+
+showHighScores = function() {
+    // run the function loadUserScores
+    loadUserScores();
+    // clear out container and footer
+    divWrapperEl.replaceChildren();
+    quizFooterEl.replaceChildren();
+    // dynamically create <h1> element
+    var pageTitle = document.createElement("h1");
+    pageTitle.className = "title-question";
+    pageTitle.innerText = "High Scores List";
+    // dynamically create <ol> element
+    var pageList = document.createElement("ol");
+    // sort the user score array using the property score
+    userScoreArray.sort((a, b) => parseInt(b.score) - parseInt(a.score));
+    // splice the array to show just the top 5 scores
+    if (userScoreArray.length > 5) {
+        userScoreArray.splice(5, userScoreArray.length);
+    }
+    //for each object in array, do the following:
+    userScoreArray.forEach(object => {
+        var pageListItem = document.createElement("li");
+        pageListItem.className = "style-top-score-item";
+        pageListItem.innerText = object.name + ' ' + object.score;
+        pageList.append(pageListItem);
+    })
+    // create buttons
+    var goBackButton = document.createElement("button");
+    goBackButton.className = "button-start-quiz hand-pointer background-change";
+    goBackButton.setAttribute("id", "id-go-back");
+    goBackButton.innerText = "Go Back to Main Page";
+    goBackButton.addEventListener('click', goToMainPage);
+
+    var clearScoresButton = document.createElement("button");
+    clearScoresButton.className = "button-start-quiz hand-pointer background-change";
+    clearScoresButton.setAttribute("id", "id-clear-scores");
+    clearScoresButton.innerText = "Clear High Scores";
+    clearScoresButton.addEventListener('click', () => {
+        localStorage.setItem("savedScores", "");
+        console.log("From showHighScores(): User scores cleared in local storage");
+        goToMainPage();
+    });
+    // append the title, list, and buttons to the container
+    divWrapperEl.append(pageTitle, pageList, goBackButton, clearScoresButton);
+}
+
+
 // event listeners
-// on click of start button, run callback handleStartQuiz()
-startButtonEl.addEventListener('click', handleStartQuiz);
-// on click of start button, begin timer
+// on click of start button, run callback startQuiz
+startButtonEl.addEventListener('click', startQuiz);
+// on click of start button, run callback beginTimer
 startButtonEl.addEventListener('click', beginTimer);
-// on click of any <li> element, run callback gradeSelectedChoice()
+// on click of any <li> element, run callback gradeSelectedChoice
 choiceListEl.addEventListener('click', gradeSelectedChoice);
-// on click of submit button, run callback saveUserInfo()
-quizWrapperEl.addEventListener('click', saveUserScore);
+// on click of submit button, run callback saveUserScore
+divWrapperEl.addEventListener('click', saveUserScore);
+// on click of <p> element in header, run callback showHighScores
+viewHighScoreEl.addEventListener('click', showHighScores)
+
 
 storeFileData();
 
